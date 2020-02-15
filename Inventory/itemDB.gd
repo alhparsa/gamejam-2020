562f4060d41bcab9b5f96cec7440c39bb611731a
junokims <juno.jaekwon.kim@gmail.com>
2020-02-15 08:20:29 +0000

    Inventory

---
+++ Inventory/itemDB.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var itemDB : Dictionary 
+func _ready() :
+		 
+	itemDB = {
+		"Water Bottle" : Item.new("Water Bottle", "Health", 1, \
+		{"Hydration": 10 }, 5.00, {}, 10, 10), 
+		"Palm Tree" : Item.new("Palm Tree", "Plant", 500.0,\
+		{"Tree": 0}, 0, {}, -1, 100),
+		"Cactus" : Item.new("Cactus", "Animal", 5,\
+		{"Plant": 10}, 2.00, {}, 20, 100),
+		"Dates" : Item.new("Fig", "Food", 1, {"Health": 5}, 1.0, \
+		{}, 10, 5), 
+		"Camel Milk" : Item.new("Camel Milk", "Food", 2, {"Health": 5}, 5.0, \
+		{}, 5, 10),
+		"Torch" : Item.new("Torch", "Weapon", 5, {"Weapon": 10}, 10.0, \
+		{}, 10, 10)}
+		
