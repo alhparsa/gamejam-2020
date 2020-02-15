96b4104240c1b9067bbffa282fb6e7c989653407
Parsa Alamzadeh <alh.parsa@gmail.com>
2020-02-15 05:03:09 -0800

    inventory added (cam god)

--- Inventory/itemDB.gd
+++ Inventory/itemDB.gd
@@ -10,7 +10,7 @@ func _ready() :
 		{"Tree": 0}, 0, {}, -1, 100),
 		"Cactus" : Item.new("Cactus", "Animal", 5,\
 		{"Plant": 10}, 2.00, {}, 20, 100),
-		"Dates" : Item.new("Fig", "Food", 1, {"Health": 5}, 1.0, \
+		"Dates" : Item.new("Dates", "Food", 1, {"Health": 5}, 1.0, \
 		{}, 10, 5), 
 		"Camel Milk" : Item.new("Camel Milk", "Food", 2, {"Health": 5}, 5.0, \
 		{}, 5, 10),
