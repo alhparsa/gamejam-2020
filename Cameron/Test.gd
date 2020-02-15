cc54b2643c6480657c9b33ca5d05586840724e7f
Cloud7831 <JaydenMcPhedrain@gmail.com>
2020-02-15 11:53:07 -0800

    added a date

--- Cameron/Test.gd
+++ Cameron/Test.gd
@@ -1,5 +1,6 @@
 extends Node
-var pickedUp = false
+var pickedUpWater = false
+var pickedUpDates = false
 
 var camel_stats : CamelStats
 var b = []
@@ -9,9 +10,13 @@ func _ready():
 	camel_stats.hp += 10
 
 func _process(delta):
-	if $TileMap.world_to_map($Player.global_position) == Vector2(19,-8) && not pickedUp:
+	if $TileMap.world_to_map($Player.global_position) == Vector2(19,-8) && not pickedUpWater:
 		$Inventory.add_item("Water Bottle")
-		pickedUp = true
+		pickedUpWater = true
+		
+	if $TileMap.world_to_map($Player.global_position) == Vector2(14,-4) && not pickedUpDates:
+		$Inventory.add_item("Dates")
+		pickedUpDates = true
 	
 
 func hi():
