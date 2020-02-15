b2eca0d18af6471bf3eb1231a0d32a19df344680
Cloud7831 <JaydenMcPhedrain@gmail.com>
2020-02-15 14:26:12 -0800

    everything works

--- Cameron/Test.gd
+++ Cameron/Test.gd
@@ -8,20 +8,19 @@ func _ready():
 	camel_stats = CamelStats.new()
 	camel_stats.connect("on_hp_change", self, "hi")
 	camel_stats.hp += 10
-	#$Waterbottle.visible = true
-	#$Waterbottle.set_global_position(Vector2(19,-8))
 	
 func _process(delta):
-	if $TileMap.world_to_map($Player.global_position) == Vector2(0,-2) && not pickedUpWater:
+	if $TileMap.world_to_map($Player.global_position) == Vector2(19,-8) && not pickedUpWater:
 		$Inventory.add_item("Water Bottle")
 		$wBottle.visible = false
 		#queue_free(wBottle)
 		pickedUpWater = true
 		
-	if $TileMap.world_to_map($Player.global_position) == Vector2(1,2) && not pickedUpDates:
+	if $TileMap.world_to_map($Player.global_position) == Vector2(17,-6) && not pickedUpDates:
 		$Inventory.add_item("Dates")
+		$Dates.visible = false;
 		pickedUpDates = true
 	
 
 func hi():
-	print("FUCK YOU")
+	print("Simran Style")
