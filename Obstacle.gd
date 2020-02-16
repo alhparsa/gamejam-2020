18f010e32616a18123eefd7b8c62387cb8157178
Ryan <swatdem12@yahoo.ca>
2020-02-16 00:14:11 -0800

    created level1 and added obstacle class

--- Obstacle.gd
+++ Obstacle.gd
@@ -1,9 +1,29 @@
-extends CollisionShape2D
-var isCleared : bool
+extends Node2D
+
+class_name Obstacle
+
+var isCleared : bool setget set_cleared, get_cleared
 var isDeadly : bool
-func _ready():
+var obstacletype1
+var obstacletype2
+signal on_clicked
+
+func init(deadly, obstacle_type1, obstacle_type2) -> void:
+	self.isDeadly = deadly
 	isCleared = false
+	obstacletype1 = obstacle_type1
+	obstacletype2 = obstacle_type2
+	$Sprite.texture = load(obstacletype1)
+
+func set_cleared(cleared : bool):
+	isCleared = cleared
+
+func get_cleared() -> bool:
+	return isCleared
+
+func onClick() -> void:
+	$Sprite.texture = load(obstacletype2)
 	
-	pass
-
-
+func _ready():
+	connect("on_clicked", self, "onClick")
+	
