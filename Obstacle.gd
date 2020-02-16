6f47aff85d98c6fcad3c06c59c6a996f3b1ac90c
Parsa Alamzadeh <alh.parsa@gmail.com>
2020-02-16 11:02:15 -0800

    level 1 works

--- Obstacle.gd
+++ Obstacle.gd
@@ -1,9 +1,34 @@
-extends CollisionShape2D
-var isCleared : bool
+extends Node2D
+
+class_name Obstacle
+
 var isDeadly : bool
-func _ready():
+var isCleared : bool
+var obstacletype1
+var obstacletype2
+
+signal on_clicked
+
+func init(deadly : bool, obstacle_type1, obstacle_type2) -> void:
+	isDeadly = deadly
 	isCleared = false
+	obstacletype1 = obstacle_type1
+	obstacletype2 = obstacle_type2
 	
-	pass
+	$Sprite.texture = load(obstacletype1)
+	
+func _process(delta):
+	var pz = get_parent().find_node("Player")
+	z_index = pz.z_index - 1 if global_position.y < pz.global_position.y else pz.z_index + 1
 
+func set_cleared(cleared : bool):
+	isCleared = cleared
 
+func get_cleared() -> bool:
+	return isCleared
+
+func onClick() -> void:
+	$Sprite.texture = load(obstacletype2)
+	
+func _ready():
+	connect("on_clicked", self, "onClick")
